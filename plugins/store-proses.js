03b6a0d8a996d690416b836c74ad121f6e62db33
X <159207241+XM4ZE@users.noreply.github.com>
2025-01-07 11:05:44 +0700

    Update store-proses.js

--- plugins/store-proses.js
+++ plugins/store-proses.js
@@ -5,7 +5,7 @@ import moment from 'moment-timezone';
 let handler = async (m, { conn, usedPrefix: _p, args, command, text }) => {
   let waktu = moment().tz('Asia/Jakarta');
   let tampilTanggal = waktu.format('dddd DD MMMM YYYY');
-  let tampilWaktu = `Jam : ${waktu.format('HH:mm:ss')}`;
+  let tampilWaktu = `${waktu.format('HH:mm:ss')}`;
   let who;
   if (m.isGroup) who = m.mentionedJid0 ? m.mentionedJid0 : m.quoted ? m.quoted.sender : text;
   else who = m.chat;
